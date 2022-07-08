2e0d3f6e01649c3d487f07f0c45621438ffacf1d
Onder Bakirtas <onderbakirtas@gmail.com>
2022-07-08 21:55:14 +0300

    feat: add collapse

---
+++ src/lib/Collapse.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { slide, fade } from 'svelte/transition';
+
+	export let open: boolean = false;
+</script>
+
+{#if open}
+	<div class="collapse " transition:slide={{ duration: 250 }}>
+		<slot />
+	</div>
+{/if}
+
+<style lang="scss">
+	.collapse {
+		display: block;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,10 @@
 <script>
 	import Button from '$lib/Button.svelte';
+	import Collapse from '$lib/Collapse.svelte';
 	import Input from '$lib/Input.svelte';
-import Switch from '$lib/Switch.svelte';
+	import Switch from '$lib/Switch.svelte';
+
+	let collapseOpened = false;
 </script>
 
 <h1>Welcome to Upo</h1>
@@ -36,17 +39,46 @@ import Switch from '$lib/Switch.svelte';
 </fieldset>
 
 <fieldset>
-  <legend>Switches</legend>
-  <div class="horizontal">
-    <Switch label="I like to tap switches" />
-    <Switch label="Hooray, another switch to tap" rounded />
-  </div>
+	<legend>Switches</legend>
+	<div class="horizontal">
+		<Switch label="I like to tap switches" />
+		<Switch label="Hooray, another switch to tap" rounded />
+	</div>
+</fieldset>
+
+<fieldset>
+	<legend>Collapse</legend>
+	<div class="horizontal">
+		<div>
+			<Button
+				text="Toggle collapse"
+				theme="blue"
+				on:click={() => (collapseOpened = !collapseOpened)}
+			/>
+			<Collapse open={collapseOpened}>
+				<p class="collapse-content">
+					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos dolor corrupti aut animi
+					cumque, voluptate quasi recusandae accusamus ratione omnis aliquid et? Et ex voluptate
+					ipsam expedita atque sed tenetur? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Cumque quos ipsa architecto asperiores accusamus dolorum illo nesciunt, tempore dicta
+					libero cupiditate quae suscipit nemo iste at, maxime illum. Modi, quas! Lorem, ipsum dolor
+					sit amet consectetur adipisicing elit. Atque fuga quos vitae suscipit quas necessitatibus
+					asperiores tempora mollitia. Qui at, neque facilis aspernatur velit consequuntur tenetur
+					nulla labore deleniti dolores.
+				</p>
+			</Collapse>
+		</div>
+	</div>
 </fieldset>
 
 <style lang="scss">
 	:global(*) {
 		box-sizing: border-box;
 	}
+
+  .collapse-content {
+    margin: 0;
+  }
 
 	fieldset {
 		padding: 1rem;
