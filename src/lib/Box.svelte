3b807bb49b2ca13f744dcaa81faa4525b1c7bfd4
Onder Bakirtas <onderbakirtas@gmail.com>
2022-07-10 14:33:42 +0300

    feat: add box component

---
+++ src/lib/Box.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import type { Size } from './types';
+
+	export let spacing: Size = 'medium';
+	export let width: string = 'auto';
+</script>
+
+<div class="box spacing-{spacing}" style:width>
+	<slot />
+</div>
+
+<style lang="scss">
+	.box {
+		background-color: white;
+		border-radius: 4px;
+    box-shadow: 0 5px 20px lightgray;
+
+		&.spacing {
+			&-tiny {
+				padding: 10px;
+			}
+
+			&-small {
+				padding: 15px;
+			}
+
+			&-medium {
+				padding: 25px;
+			}
+
+			&-large {
+				padding: 40px;
+			}
+
+			&-xlarge {
+				padding: 50px;
+			}
+		}
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Box from '$lib/Box.svelte';
 	import Button from '$lib/Button.svelte';
 	import Collapse from '$lib/Collapse.svelte';
 	import Input from '$lib/Input.svelte';
@@ -9,75 +10,108 @@
 
 <h1>Welcome to Upo</h1>
 
-<fieldset>
-	<legend>Button Themes</legend>
-	<div class="buttons">
-		<Button text="Blue" theme="blue" />
-		<Button text="Green" theme="green" />
-		<Button text="Red" theme="red" />
-		<Button text="Orange" theme="orange" />
-		<Button text="Default" theme="default" />
-	</div>
-</fieldset>
-
-<fieldset>
-	<legend>Button Sizes</legend>
-	<div class="buttons">
-		<Button text="Small" size="small" theme="blue" />
-		<Button text="Medium" size="medium" theme="blue" />
-		<Button text="Large" size="large" theme="blue" />
-	</div>
-</fieldset>
-
-<fieldset>
-	<legend>Inputs</legend>
-	<div class="horizontal">
-		<Input size="small" />
-		<Input size="medium" />
-		<Input size="large" />
-	</div>
-</fieldset>
-
-<fieldset>
-	<legend>Switches</legend>
-	<div class="horizontal">
-		<Switch label="I like to tap switches" />
-		<Switch label="Hooray, another switch to tap" rounded />
-	</div>
-</fieldset>
-
-<fieldset>
-	<legend>Collapse</legend>
-	<div class="horizontal">
-		<div>
-			<Button
-				text="Toggle collapse"
-				theme="blue"
-				on:click={() => (collapseOpened = !collapseOpened)}
-			/>
-			<Collapse open={collapseOpened}>
-				<p class="collapse-content">
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos dolor corrupti aut animi
-					cumque, voluptate quasi recusandae accusamus ratione omnis aliquid et? Et ex voluptate
-					ipsam expedita atque sed tenetur? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					Cumque quos ipsa architecto asperiores accusamus dolorum illo nesciunt, tempore dicta
-					libero cupiditate quae suscipit nemo iste at, maxime illum. Modi, quas! Lorem, ipsum dolor
-					sit amet consectetur adipisicing elit. Atque fuga quos vitae suscipit quas necessitatibus
-					asperiores tempora mollitia. Qui at, neque facilis aspernatur velit consequuntur tenetur
-					nulla labore deleniti dolores.
-				</p>
-			</Collapse>
+<div class="content">
+	<fieldset>
+		<legend>Button Themes</legend>
+		<div class="buttons">
+			<Button text="Blue" theme="blue" />
+			<Button text="Green" theme="green" />
+			<Button text="Red" theme="red" />
+			<Button text="Orange" theme="orange" />
+			<Button text="Default" theme="default" />
 		</div>
-	</div>
-</fieldset>
+	</fieldset>
+
+	<fieldset>
+		<legend>Button Sizes</legend>
+		<div class="buttons">
+			<Button text="Small" size="small" theme="blue" />
+			<Button text="Medium" size="medium" theme="blue" />
+			<Button text="Large" size="large" theme="blue" />
+		</div>
+	</fieldset>
+
+	<fieldset>
+		<legend>Inputs</legend>
+		<div class="horizontal">
+			<Input size="small" />
+			<Input size="medium" />
+			<Input size="large" />
+		</div>
+	</fieldset>
+
+	<fieldset>
+		<legend>Switches</legend>
+		<div class="horizontal">
+			<Switch label="I like to tap switches" />
+			<Switch label="Hooray, another switch to tap" rounded />
+		</div>
+	</fieldset>
+
+	<fieldset>
+		<legend>Collapse</legend>
+		<div class="horizontal">
+			<div>
+				<Button
+					text="Toggle collapse"
+					theme="blue"
+					on:click={() => (collapseOpened = !collapseOpened)}
+				/>
+				<Collapse open={collapseOpened}>
+					<p class="collapse-content">
+						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos dolor corrupti aut animi
+						cumque, voluptate quasi recusandae accusamus ratione omnis aliquid et? Et ex voluptate
+						ipsam expedita atque sed tenetur? Lorem ipsum dolor sit amet consectetur adipisicing
+						elit. Cumque quos ipsa architecto asperiores accusamus dolorum illo nesciunt, tempore
+						dicta libero cupiditate quae suscipit nemo iste at, maxime illum. Modi, quas! Lorem,
+						ipsum dolor sit amet consectetur adipisicing elit. Atque fuga quos vitae suscipit quas
+						necessitatibus asperiores tempora mollitia. Qui at, neque facilis aspernatur velit
+						consequuntur tenetur nulla labore deleniti dolores.
+					</p>
+				</Collapse>
+			</div>
+		</div>
+	</fieldset>
+
+	<fieldset>
+		<legend>Box</legend>
+		<div class="horizontal">
+			<Box>
+				<div>
+					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et nihil inventore similique
+					enim tenetur ipsam, placeat debitis temporibus accusantium, minus exercitationem,
+					laudantium numquam repellat tempore voluptas eum velit sint ullam? Lorem ipsum dolor, sit
+					amet consectetur adipisicing elit. Eveniet suscipit ipsa quod quo provident iste, a soluta
+					autem commodi voluptas ex ducimus est unde voluptatibus ad error! Harum, laborum facere!
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum animi rem quos. Deserunt,
+					possimus dolorem quia quis ratione ad sed voluptatem eligendi totam distinctio accusantium
+					corporis magni velit voluptates hic.
+				</div>
+			</Box>
+			<Box spacing="large" width="320px">
+				<div>
+					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et nihil inventore similique
+					enim tenetur ipsam, placeat debitis temporibus accusantium, minus exercitationem,
+					laudantium numquam repellat tempore voluptas eum velit sint ullam? Lorem ipsum dolor, sit
+					amet consectetur adipisicing elit. Eveniet suscipit ipsa quod quo provident iste, a soluta
+					autem commodi voluptas ex ducimus est unde voluptatibus ad error! Harum, laborum facere!
+				</div>
+			</Box>
+		</div>
+	</fieldset>
+</div>
 
 <style lang="scss">
 	:global(*) {
 		box-sizing: border-box;
 	}
 
-  .collapse-content {
-    margin: 0;
+	.collapse-content {
+		margin: 0;
+	}
+
+  .content {
+    padding: 20px 0 100px;
   }
 
 	fieldset {
