f77bad109e8b2fba28904d7e86e55ff371dedcee
Onder Bakirtas <onderbakirtas@gmail.com>
2022-07-10 16:52:20 +0300

    fix: optimized collapse transition

--- src/lib/Collapse.svelte
+++ src/lib/Collapse.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
 	import { slide, fade } from 'svelte/transition';
+  import { linear } from 'svelte/easing'
 
 	export let open: boolean = false;
 </script>
 
 {#if open}
-	<div class="collapse " transition:slide={{ duration: 250 }}>
-		<slot />
+	<div class="collapse" transition:fade={{ duration: 200, easing: linear }}>
+		<div transition:slide={{ duration: 250 }}>
+			<slot />
+		</div>
 	</div>
 {/if}
 
