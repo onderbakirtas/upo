c2f04be7e2b0f6b6c410db7f3a7305fe0895b02e
Onder Bakirtas <onderbakirtas@gmail.com>
2022-07-11 14:41:21 +0300

    fix: no exports was present for certain components

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,8 @@
-import "./styles/core.scss";
+import './styles/core.scss';
 
 export { default as Button } from './Button.svelte';
+export { default as Input } from './Input.svelte';
+export { default as Collapse } from './Collapse.svelte';
+export { default as Box } from './Box.svelte';
+export { default as Label } from './Label.svelte';
+export { default as Switch } from './Switch.svelte';
